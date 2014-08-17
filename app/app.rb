7ef65c8795a0fae11976ed3ebb890a5ec3e70839
Kazunori Jo <itochan315@gmail.com>
2014-08-17 20:10:45 +0900

    add play feature

--- app/app.rb
+++ app/app.rb
@@ -27,9 +27,12 @@ module Ohayo
       case params['Digits']
       when '1'
         response = Twilio::TwiML::Response.new do |r|
-          r.Say "録音します。終わるときはいずれかのキーを押してください。"
+          r.Say "録音します。終わるときはいずれかのキーを押してください。", language: "ja-jp", voice: "woman"
           r.Record maxLength: '10', action: '/call/handle-record', method: 'get'
+          r.Redirect 'menu', method: 'get'
         end
+      when '2'
+        redirect '/call/play/menu'
       else
         redirect '/call/menu'
       end
@@ -47,9 +50,56 @@ module Ohayo
       record.save
 
       Twilio::TwiML::Response.new do |r|
-        r.Say "録音が終わりました。再生します。"
+        r.Say "録音が終わりました。再生します。", language: "ja-jp", voice: "woman"
         r.Play recording_url
-        r.Say "ありがとうございました"
+        r.Say "ありがとうございました", language: "ja-jp", voice: "woman"
+      end.text
+    end
+
+    get '/call/play/menu' do
+      content_type 'text/xml'
+
+      Twilio::TwiML::Response.new do |r|
+        r.Gather numDigits: '1', action: '/call/play/handle-gather', method: 'get' do |g|
+          g.Say "最新の音声を再生するには1を、そのほかの音声を再生するには2を押してください。", language: "ja-jp", voice: "woman"
+        end
+      end.text
+    end
+
+    get '/call/play/handle-gather' do
+      content_type 'text/xml'
+
+      case params['Digits']
+      when '1'
+        response = Twilio::TwiML::Response.new do |r|
+          r.Say "最新の音声を再生します。", language: "ja-jp", voice: "woman"
+          r.play Records.last.url
+          r.Redirect "menu", method: 'get'
+        end
+      when '2'
+        response = Twilio::TwiML::Response.new do |r|
+          r.Gather action: '/call/play/choose/handle-gather', method: 'get' do |g|
+            g.Say "再生したい番号を押して、最後にシャープを押してください。", language: "ja-jp", voice: "woman"
+          end
+        end
+      else
+        response = Twilio::TwiML::Response.new do |r|
+          r.Redirect '/call/menu', method: 'get'
+        end
+      end
+
+      response.text
+    end
+
+    get '/call/play/choose/handle-gather' do
+      content_type 'text/xml'
+
+      number = params['Digits']
+
+      Twilio::TwiML::Response.new do |r|
+        r.Say "#{number}番の音声を再生します。", language: "ja-jp", voice: "woman"
+        r.play Records.find_by(number).url
+        r.Redirect '../menu', method: 'get'
       end.text
     end
   end
